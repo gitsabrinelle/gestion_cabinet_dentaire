bb10560e17ed88db1efd3c7334a2fc9280a626a7
kremairiracha <kremairiracha@gmail.com>
2021-03-03 22:22:17 +0400

    pas la peine de te mettre un commentaire xD

--- model/global.php
+++ model/global.php
@@ -1,5 +1,5 @@
 <?php
-$db = 'stock_23';                             
+$db = 'stock_2021';                             
 $link = mysqli_connect('localhost:3306', 'root', '') or header("Location:gestion/installation/index.php");
 
 mysqli_select_db($link ,$db) or header("Location:gestion/installation/index.php");;
--- structure/scripts.php
+++ structure/scripts.php
@@ -38,3 +38,15 @@
 
 <!-- Demo Settings -->
 <script src="assets/js/neon-demo.js"></script>
+
+<!-- js_2 -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+	<script src="src/plugins/apexcharts/apexcharts.min.js"></script>
+	<script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+	<script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+	<script src="vendors/scripts/dashboard.js"></script>
